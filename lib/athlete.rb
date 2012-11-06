8cd7fc97137cf30dc2adf43cf9b469c33f8fd715
Justin Ramel <justin.ramel@gmail.com>
2012-11-06 23:50:14 +0000

    refactoring strava gateway

--- lib/athlete.rb
+++ lib/athlete.rb
@@ -2,8 +2,26 @@ module Jersey
   class Athlete
     attr_accessor :period, :name, :miles, :hours, :minutes, :feet
 
+    def validate!
+      period_start = period.split(/ - /)[0]
+      if (period_start != last_monday)
+        self.miles   = 0
+        self.hours   = 0
+        self.minutes = 0
+        self.feet    = 0
+      end
+      self
+    end
+
     def to_s
       "#{period}\t#{name}\t#{miles}mi\t#{hours}hr#{minutes}m\t#{feet}ft"
     end
+
+    private
+
+    def last_monday
+      DateTime.parse("Monday").strftime("%b %d, %Y")
+    end
+
   end
 end
--- lib/strava_gateway.rb
+++ lib/strava_gateway.rb
@@ -7,35 +7,28 @@ module Jersey
 
     def self.get_athlete(id)
       response = Faraday.get("http://www.strava.com/athletes/#{id}")
-      body     = response.body
-      period   = body.slice(/<h2 id='interval-value'>Activities for (.+?)\n/, 1)
-      name     = body.slice(/<h1 class='topless' id='athlete-name'>(.+?)</,   1)
-      miles    = body.slice(/<strong>(.+?)<abbr class='unit' title='miles'>/, 1)
-      hours    = body.slice(/<strong>(.+?)<abbr class='unit' title='hours'>/, 1)
-      minutes  = body.slice(/abbr>(.+?)<abbr class='unit' title='minutes'>/,  1)
-      feet     = body.slice(/<strong>(.+?)<abbr class='unit' title='feet'>/,  1)
-      feet     = feet.sub(',', '').to_i
+      data     = parse(response.body)
 
-      period_start = period.split(/ - /)[0]
-      if (period_start != self.last_monday)
-        miles   = 0
-        hours   = 0
-        minutes = 0
-        feet    = 0
-      end
-
-      Athlete.new.tap do |a|
-        a.period  = period
-        a.name    = name
-        a.miles   = miles
-        a.hours   = hours
-        a.minutes = minutes
-        a.feet    = feet
-      end
+      athlete = Athlete.new
+      athlete.period  = data[:period]
+      athlete.name    = data[:name]
+      athlete.miles   = data[:miles]
+      athlete.hours   = data[:hours]
+      athlete.minutes = data[:minutes]
+      athlete.feet    = data[:feet]
+      athlete.validate!
     end
 
-    def self.last_monday
-      DateTime.parse("Monday").strftime("%b %d, %Y")
+    def self.parse(body)
+      h = {}
+      h[:period]  = body.slice(/>Activities for (.+?)\n/, 1)
+      h[:name]    = body.slice(/id='athlete-name'>(.+?)</,   1)
+      h[:miles]   = body.slice(/>(.+?)<abbr class='unit' title='miles'>/, 1)
+      h[:hours]   = body.slice(/>(.+?)<abbr class='unit' title='hours'>/, 1)
+      h[:minutes] = body.slice(/>(.+?)<abbr class='unit' title='minutes'>/,  1)
+      h[:feet]    = body.slice(/>(.+?)<abbr class='unit' title='feet'>/,  1)
+      h[:feet ]   = h[:feet].sub(',', '').to_i
+      h
     end
 
   end
